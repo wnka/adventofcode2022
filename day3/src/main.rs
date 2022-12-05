ff35a7e2ce71bd662646333d28734755ae56e399
Phil Piwonka <wnka@users.noreply.github.com>
2022-12-05 08:04:15 -0800

    try one but this sets lib sucks
    the intersection will return duplicates!

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,6 +1,7 @@
-use std::{fs::File, io::{BufRead, BufReader}, fmt};
+use std::{fs::File, io::{BufRead, BufReader}};
 
 use clap::Parser;
+use sets::{Set,MutSetOps};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -13,15 +14,40 @@ struct Args {
 
 #[derive(Debug)]
 enum ParseError {
-    Error
+    Error,
+    NotEven
 }
 
-fn parse<T>(input_buffer: T) -> Result<Vec<String>, ParseError> where T: BufRead {
+struct Rucksack {
+    one: String,
+    two: String,
+}
+
+impl Rucksack {
+    fn common_elements(&self) -> Vec<char> {
+        let mut retval = Vec::new();
+        let set_one = Set::new_ordered(self.one.as_bytes(), true);
+        let set_two = Set::new_ordered(self.two.as_bytes(), true);
+        let sames = set_one.intersection(&set_two);
+        for item in sames.data {
+            retval.push(char::from(item));
+        }
+        retval
+    }
+}
+
+fn parse<T>(input_buffer: T) -> Result<Vec<Rucksack>, ParseError> where T: BufRead {
     let mut ranges = Vec::new();
     let lines = BufReader::new(input_buffer).lines();
     for line in lines {
         match line {
-            Ok(s) => ranges.push(s),
+            Ok(s) => {
+                if s.len() % 2 != 0 { return Err(ParseError::NotEven) }
+                let size = s.len()/2;
+                let one = &s[0..size];
+                let two = &s[size..s.len()];
+                ranges.push(Rucksack{one:one.to_string(), two:two.to_string()});
+            }
             Err(_) => return Err(ParseError::Error)
         }
     }
@@ -32,11 +58,25 @@ fn main() -> Result<(), ParseError> {
     let args = Args::parse();
 
     let input_file = File::open(args.input).unwrap();
-    let input_ranges = parse(BufReader::new(input_file))?;
+    let rucksacks = parse(BufReader::new(input_file))?;
 
-    for range in input_ranges {
-        println!("{}", range);
+    let mut score: usize = 0;
+
+    for sack in rucksacks {
+        let common = sack.common_elements();
+        println!("{},{} = {:?}", sack.one, sack.two, common);
+        for ch in common {
+            let uch = ch as u8 as usize;
+            if (65..=90).contains(&uch) {
+                score += uch - 38;
+            } else if (97..=122).contains(&uch) {
+                score += uch - 96;
+            }
+            println!("Value = {}", score);
+        }
     }
+
+    // initial guess was 8966 ... ohhh I didn't dedupe
 
     Ok(())
 }
