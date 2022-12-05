2539adf800e4c55079eb6348c5ee22c9450caf63
Phil Piwonka <wnka@users.noreply.github.com>
2022-12-05 08:04:15 -0800

    day 3 done!

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,7 +1,8 @@
-use std::{fs::File, io::{BufRead, BufReader}};
+use std::{fs::File, io::{BufRead, BufReader, Seek}};
 
 use clap::Parser;
 use std::collections::HashSet;
+use itertools::Itertools;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -16,6 +17,30 @@ struct Args {
 enum ParseError {
     Error,
     NotEven
+}
+
+struct ElfGroup {
+    one: String,
+    two: String,
+    three: String,
+}
+
+impl ElfGroup {
+    fn common_element(&self) -> Vec<char> {
+        let mut retval = Vec::new();
+        let mut set_one: HashSet<char> = HashSet::from_iter(self.one.chars());
+        let set_two: HashSet<char> = HashSet::from_iter(self.two.chars());
+        let set_three: HashSet<char> = HashSet::from_iter(self.three.chars());
+
+        // Dude this sucks, but it seems to work.
+        let intersection = &mut set_one;
+        intersection.retain(|e| set_two.contains(e));
+        let sames : Vec<&char> = intersection.intersection(&set_three).collect();
+        for ch in sames {
+            retval.push(*ch);
+        }
+        retval
+    }
 }
 
 struct Rucksack {
@@ -54,29 +79,57 @@ fn parse<T>(input_buffer: T) -> Result<Vec<Rucksack>, ParseError> where T: BufRe
     Ok(ranges)
 }
 
+fn parseElfGroup<T>(input_buffer: T) -> Result<Vec<ElfGroup>, ParseError> where T: BufRead {
+    let mut groups = Vec::new();
+    let lines = BufReader::new(input_buffer).lines();
+    for chunk in &lines.into_iter().chunks(3) {
+        if let Some((a,b,c)) = chunk.collect_tuple() {
+            groups.push(ElfGroup{one:a.unwrap(), two:b.unwrap(), three:c.unwrap()})
+        }
+    }
+    Ok(groups)
+}
+
+fn score_char(input: char) -> usize {
+    let uch = input as u8 as usize;
+    if (65..=90).contains(&uch) {
+        uch - 38
+    } else if (97..=122).contains(&uch) {
+        uch - 96
+    } else {
+        0
+    }
+
+}
+
 fn main() -> Result<(), ParseError> {
     let args = Args::parse();
 
-    let input_file = File::open(args.input).unwrap();
+    let input_file = File::open(&args.input).unwrap();
     let rucksacks = parse(BufReader::new(input_file))?;
 
     let mut score: usize = 0;
 
     for sack in rucksacks {
         let common = sack.common_elements();
-        println!("{},{} = {:?}", sack.one, sack.two, common);
         for ch in common {
-            let uch = ch as u8 as usize;
-            if (65..=90).contains(&uch) {
-                score += uch - 38;
-            } else if (97..=122).contains(&uch) {
-                score += uch - 96;
-            }
-            println!("Value = {}", score);
+            score += score_char(ch);
+        }
+    }
+    println!("Value = {}", score);
+
+    let input_file = File::open(args.input).unwrap();
+    let groups = parseElfGroup(BufReader::new(input_file));
+
+    let mut group_score = 0;
+
+    for group in groups.unwrap() {
+        for ch in group.common_element() {
+            group_score += score_char(ch);
         }
     }
 
-    // initial guess was 8966 ... ohhh I didn't dedupe
+    println!("Group Score: {}", group_score);
 
     Ok(())
 }
